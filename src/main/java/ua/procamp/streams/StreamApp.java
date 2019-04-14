fb08a468a5ccb0b1e3c0b9abf72a53aa3345c64a
Stanislav <stasn@ua.fn>
2019-04-14 08:04:55 +0300

    add test

--- src/main/java/ua/procamp/streams/StreamApp.java
+++ src/main/java/ua/procamp/streams/StreamApp.java
@@ -7,12 +7,10 @@ import ua.procamp.streams.stream.IntStream;
 public class StreamApp {
 
     public static int streamOperations(IntStream intStream) {
-//        IntStream intStream = AsIntStream.of(-1, 0, 1, 2, 3); // input values
         int res = intStream
                 .filter(x -> x > 0) // 1, 2, 3
                 .map(x -> x * x) // 1, 4, 9
                 .flatMap(x -> AsIntStream.of(x - 1, x, x + 1)) // 0, 1, 2, 3, 4, 5, 8, 9, 10
-//                .sum();
                 .reduce(0, (a, b) -> a + b); // 42
         return res;
     }
